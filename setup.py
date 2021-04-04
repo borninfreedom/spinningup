e86567b819f61f7bd42eb4691b96877c570b18d4
Yan Wen <ericwen657@foxmail.com>
2021-04-04 20:17:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,10 @@ setup(
         'seaborn==0.8.1',
         'tensorflow>=1.8.0,<2.0',
         'torch==1.3.1',
+        'torchvision==0.4.2',
+        'colorama==0.4.4',
+        'pybullet==3.1.0',
+        
         'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
